0585cabb9c3b44fd0abf54683388e820cd2c09e9
Harjot Singh <88283165+Harjot01@users.noreply.github.com>
2024-08-01 16:31:17 +0530

    Update script.js

--- Tic Tac Toe/script.js
+++ Tic Tac Toe/script.js
@@ -1,104 +1,85 @@
-.ui {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+const cells = document.querySelectorAll(".cell");
+const resetButton = document.getElementById("btn");
+const printText = document.getElementById("print");
+console.log(cells);
+flag = 1;
+
+let board = ["", "", "", "", "", "", "", "", ""];
+
+resetButton.addEventListener("click", (e) => {
+  location.reload();
+});
+
+printText.innerText = "Player X Turn";
+
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+cells.forEach((cell, index) => {
+  cell.addEventListener("click", (e) => {
+    handleInput(e, index);
+  });
+});
+
+function handleInput(e, index) {
+  if (flag == 1) {
+    e.target.value = "X";
+    board[index] = "X";
+    printText.innerText = "Player O Turn";
+    flag = 0;
+  } else {
+    e.target.value = "O";
+    board[index] = "O";
+    printText.innerText = "Player X Turn";
+    flag = 1;
+  }
+  e.target.disabled = true;
+
+  checkWinner();
 }
 
-body {
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: #212121;
+function checkWinner() {
+  let winner = null;
 
-}
+  for (let combination of winningCombinations) {
+    const [a, b, c] = combination;
+    if (board[a] === board[b] && board[a] === board[c]) {
+      winner = board[a];
+      break;
+    }
+  }
 
-#main {
-  flex-direction: column;
-  align-items: center;
-  display: flex;
-  justify-content: center;
-}
+  if (winner) {
+    printText.innerText = `Player ${winner} won`;
+    cells.forEach((cell) => (cell.disabled = true));
+    highlightWinner(winner);
+  } else {
+    // check if all the board cells are filled that means a draw
+    let ct = 0;
+    for (let bd of board) {
+      if (bd === "X" || bd == "O") ct++;
+    }
 
-.row {
-  display: flex;
-}
-
-.cell {
-  border: none;
-  width: 120px;
-  height: 120px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 48px;
-  color: white;
-  text-align: center;
-  cursor: pointer;
-  background-color: #212121;
+    if (ct == 9) printText.innerText = `Draw between X and O`;
+  }
 }
 
-.cell:active {
-  outline: none;
-}
-
-#b1 {
-  border-bottom: 1px solid white;
-  border-right: 1px solid white;
-}
-#b2 {
-  border-bottom: 1px solid white;
-  border-right: 1px solid white;
-  border-left: 1px solid white;
-}
-#b3 {
-  border-bottom: 1px solid white;
-  border-left: 1px solid white;
-}
-#b4 {
-  border-top: 1px solid white;
-  border-bottom: 1px solid white;
-  border-right: 1px solid white;
-}
-#b5 {
-  border: 1px solid white;
-}
-#b6 {
-  border-top: 1px solid white;
-  border-bottom: 1px solid white;
-  border-left: 1px solid white;
-}
-#b7 {
-  border-top: 1px solid white;
-  border-right: 1px solid white;
-}
-#b8 {
-  border-top: 1px solid white;
-  border-right: 1px solid white;
-  border-left: 1px solid white;
-}
-#b9 {
-  border-top: 1px solid white;
-  border-left: 1px solid white;
-}
-
-#btn {
-  box-sizing: border-box;
-  width: 95px;
-  height: 40px;
-  border: 1px solid white;
-  margin-top: 50px;
-  border-radius: 8px;
-  font-family: Verdana, Geneva, Tahoma, sans-serif;
-  background-color: whitesmoke;
-  color: black;
-  font-size: 20px;
-  cursor: pointer;
-}
-
-#print {
-    margin-top: 50px;
-  font-family: Verdana, Geneva, Tahoma, sans-serif;
-  color: white;
-  font-size: 20px;
+function highlightWinner(winner) {
+  for (let combination of winningCombinations) {
+    const [a, b, c] = combination;
+    if (board[a] === winner && board[b] === winner && board[c] === winner) {
+      cells[a].style.color = "green";
+      cells[b].style.color = "green";
+      cells[c].style.color = "green";
+      break;
+    }
+  }
 }
